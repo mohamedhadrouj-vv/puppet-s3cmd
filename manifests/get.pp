deb5641d5c0e5d71661df64c3dda6747a35eaf91
mohamedhadrouj-vv <mohamed.hadrouj@viavoo.com>
2013-01-21 21:42:36 +0100

    added timeout param + logoutput = true + added s3cmd command path

--- manifests/get.pp
+++ manifests/get.pp
@@ -3,14 +3,17 @@ define s3cmd::get(
     $object,
     $local_file,
     $options,
+    $timeout = 0,
     $tries = 1,
     $try_sleep = 1,
     $config_file = '/root/.s3cfg') {
 
   exec { "s3cmd-get-$bucket-$object":
-    command => "s3cmd -c $config_file $options get s3://$bucket/$object $local_file",
-    logoutput => 'on_failure',
-    tries => $tries,
+    command   => "s3cmd -c $config_file $options get s3://$bucket/$object $local_file",
+    path      => '/usr/bin/',
+    logoutput => 'true',
+    timeout   => $timeout,
+    tries     => $tries,
     try_sleep => $try_sleep
   }
   
